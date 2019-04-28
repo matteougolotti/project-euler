ce6f5a528357aff5de02548b5e0920db14243357
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2019-04-28 23:22:41 +0200

    Code refactoring

--- 0004/P0004.py
+++ 0004/P0004.py
@@ -11,20 +11,26 @@ def has_six_digits(n):
     s = str(n)
     return len(s) == 6
 
-def count(n):
-    result = 0
+def palindromes():
+    result = set()
     for i in range(100, 1000):
         for j in range(i, 1000):
             p = i * j
-            if p >= n:
-                break
-            elif is_palindrome(p) and has_six_digits(p):
-                result = max(result, p)
+            if is_palindrome(p) and has_six_digits(p):
+                result.add(p)
+    return result
+
+def find(n, palindromes):
+    result = 0
+    for p in palindromes:
+        if p < n:
+            result = max(result, p)
     return result
 
 if __name__ == "__main__":
     t = int(input())
+    palindromes_set = palindromes()
     for i in range(t):
         n = int(input())
-        r = count(n)
+        r = find(n, palindromes_set)
         print(r)
