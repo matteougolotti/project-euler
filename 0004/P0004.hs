c7fb229068f22abe5ecee08d2a0873ec87f55597
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2019-04-28 21:37:08 +0200

    Refactoring

--- 0004/P0004.hs
+++ 0004/P0004.hs
@@ -7,9 +7,8 @@ isValidPalindrome s = length s == 6 && s == reverse s
 
 largestPalindrome :: Int -> Int
 largestPalindrome n =
-  let
-    palindromes = [ p | i <- [100..999],
-                        j <- [i..999],
-                        let p = i * j,
-                        isValidPalindrome $ show p ]
+  let  palindromes = [ p | i <- [100..999],
+                           j <- [i..999],
+                           let p = i * j,
+                           isValidPalindrome $ show p ]
   in maximum . filter (<n) $ palindromes
